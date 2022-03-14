5df2d91ad08c104b29b36245071dced9e8548dbb
Marcin Grajdek <grajdek.m@vp.pl>
2022-03-14 18:44:50 +0100

    Add AJAX&API - zd. 9.1 roboczy 1

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,9 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
+      
       
       console.log('new Product:', thisProduct);
     }
@@ -93,6 +95,7 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
     
     initAccordion(){
@@ -243,8 +246,78 @@
 
     }
 
-
+    initAmountWidget(){
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+    }
   }
+  
+  
+  class AmountWidget {
+    constructor(element){
+      const thisWidget = this;
+      
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+    }
+    
+    getElements(element){
+      const thisWidget = this;
+    
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    
+    setValue(value){
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /*TODO: Add validation */
+      if(thisWidget.value !== newValue && !isNaN(newValue)){
+        thisWidget.value = newValue;
+      }
+     
+     
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
+
+
+    }
+    initActions(){
+      const thisWidget = this;
+      
+
+      thisWidget.input.addEventListener('chage', function(event) {
+        event.preventDefault();
+        thisWidget.setValue();
+
+      });
+           
+      
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue();
+        thisWidget.value -= 1;
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue();
+        thisWidget.value += 1;
+      });
+      
+    }
+    
+  }
+
+
+
   const app = {
     initData: function(){
       const thisApp = this;
