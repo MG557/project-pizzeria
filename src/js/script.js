9543bf28df865a02942ebc9f103498c69577d6f1
Marcin Grajdek <grajdek.m@vp.pl>
2022-03-23 12:03:25 +0100

    Add CartProduct - Wiget zd. 9.5 roboczy 3

--- src/js/script.js
+++ src/js/script.js
@@ -506,9 +506,17 @@
   class CartProduct{
     constructor(menuProduct, element){
       const thisCartProduct = this;
-      
-      
+     
+
       thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.amount = menuProduct.amount;
+
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.price = menuProduct.price,
+      thisCartProduct.params = menuProduct.params,
+
+      
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
       
@@ -522,11 +530,11 @@
       thisCartProduct.dom = {
 
         wrapper: element,
-        amountWidget: element.querySelector(select.cartProduct.amountWidget),
+        amountWidgetElem: element.querySelector(select.cartProduct.amountWidget),
         price: element.querySelector(select.cartProduct.price),
         edit: element.querySelector(select.cartProduct.edit),
         remove: element.querySelector(select.cartProduct.remove),
-
+        //thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
       };
     }
     initAmountWidget(){
@@ -537,8 +545,8 @@
         //thisCartProduct.processOrder();
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         
-        thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amountWidget.value; 
-        //thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
+        //thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amountWidget.value; 
+        thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
       
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
         
