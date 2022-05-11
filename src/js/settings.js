6c026f1aea794e034b35ca9d3035b4be1780123d
Marcin Grajdek <grajdek.m@vp.pl>
2022-05-11 19:53:45 +0200

    Add Home Page - 11.4 roboczy 2

--- src/js/settings.js
+++ src/js/settings.js
@@ -3,12 +3,14 @@ export const select = {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product', 
     bookingWidget: '#template-booking-widget',
+    homePage: '#template-home-page',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
     pages: '#pages',
     booking: '.booking-wrapper',
+    home: '.home-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -154,4 +156,6 @@ export const templates = {
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   
   bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
+
+  homePage: Handlebars.compile(document.querySelector(select.templateOf.homePage).innerHTML),
 };
